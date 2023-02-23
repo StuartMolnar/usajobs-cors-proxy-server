feaf350bef4ff211e89a251c6c906de5706d707f
StuartMolnar <stuartmolnar@hotmail.com>
2023-02-23 04:21:31 -0800

    update

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Get the hostname from the configuration file
+# Get variables from the configuration file
 hostname=$(grep -oP 'hostname: \K.*' app_conf.yml)
+email=$(grep -oP 'email: \K.*' app_conf.yml)
 
 # Install necessary packages
 sudo apt-get update -y
@@ -24,7 +25,7 @@ sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
 # Request SSL/TLS certificate using Certbot
-sudo certbot certonly --standalone -d "$hostname"
+sudo certbot certonly --non-interactive --agree-tos --standalone -d "$hostname" --email "$email"
 
 # Move the Systemd service unit file and start the service
 sudo mv proxy.service /etc/systemd/system
