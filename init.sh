512ade763dcc0ef827268251a5b9a364f510acc6
StuartMolnar <stuartmolnar@hotmail.com>
2023-02-27 17:30:24 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -32,7 +32,7 @@ sudo apt-cache policy docker-ce
 sudo apt install docker-ce -y
 
 # Remove the old Docker image if it exists
-docker rmi proxy:latest
+sudo docker rmi proxy:latest
 
 # Build the Docker image with Hostname as an environment variable
 sudo docker build --build-arg HOSTNAME=$hostname-t -t proxy:latest .
